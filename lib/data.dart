bb1672112c06ed99414ca3549a39d3f04a9a23be
Pablo <pablohuggem@gmail.com>
2023-03-31 23:21:06 -0300

    feat(app): change app layout
    Signed-off-by: Pablo <pablohuggem@gmail.com>

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,34 @@
 export "data.dart" show items;
 
-List<Map> items = [
+List<Map<String, String>> items2 = [
+  {'image': "assets/images/ir_para_escola.jpeg", "desc": "Ir para a escola"},
+  {"image": "assets/images/passear.jpeg", "desc": "Passear"},
+  {"image": "assets/images/jogar_bola.jpeg", "desc": "Jogar bola"},
   {
-    'image': 'assets/images/banana.png',
-    'desc': 'Banana',
+    'image': 'assets/images/brincar.jpeg',
+    'desc': 'Brincar',
   },
   {
-    'image': 'assets/images/casa.png',
-    'desc': 'Casa',
+    'image': 'assets/images/assistir_tv.jpeg',
+    'desc': 'Assistir TV',
   },
+];
+
+List<Map<String, String>> items1 = [
+  {"image": "assets/images/levantar.jpeg", "desc": "Levantar"},
+  {
+    "image": "assets/images/dormir.jpeg",
+    "desc": "Dormir",
+  },
+  {"image": "assets/images/tomar_banho.jpeg", "desc": "Tomar banho"},
+  {
+    "image": "assets/images/ir_ao_banheiro.jpeg",
+    "desc": "Ir ao banheiro",
+  },
+  {"image": "assets/images/se_vestir.jpeg", "desc": "Se vestir"},
+  {
+    'image': 'assets/images/comer.jpeg',
+    'desc': 'Comer',
+  },
+  {"image": "assets/images/tomar_agua.jpeg", "desc": "Tomar água"}
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -3,62 +3,27 @@ import 'data.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'tts.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'CAA',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.pink,
-        ),
-        home: const MyHomePage(title: 'CAA'));
+      title: 'CAA',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const MyHomePage(title: 'CAA'),
+    );
   }
 }
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text('Testing TTS on Web')),
-//         body: Center(
-//           child: ElevatedButton(
-//             onPressed: () => speak('Hello'),
-//             child: Text('Tap to Speak'),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -67,80 +32,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/ear.png',
-                fit: BoxFit.contain,
-                height: 32,
-              ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('CAA'),
-              ),
-            ],
-          ),
-          backgroundColor: Color(0xFFE91E63), // Pink color
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/ear.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('CAA'),
+            ),
+          ],
         ),
-        body: GridView.builder(
-          itemCount: items.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: MediaQuery.of(context).size.width /
-                (MediaQuery.of(context).size.height / 3),
+        backgroundColor: Color(0xFFE91E63),
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            // Add context and column title for items1
+            child: buildGridView(context, "Cuidados/Higiene", items1),
           ),
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                speak(items[index]['desc']);
-              },
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: Image.asset(
-                        items[index]['image'],
-                        fit: BoxFit.contain,
+          Expanded(
+            // Add context and column title for items2
+            child: buildGridView(context, "Ações", items2),
+          ),
+        ],
+      ),
+    );
+  }
+
+// Add BuildContext to the buildGridView function's parameters
+  Widget buildGridView(
+      BuildContext context, String title, List<Map<String, String>> items) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        Expanded(
+          child: GridView.builder(
+            itemCount: items.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                  (MediaQuery.of(context).size.height / 2),
+            ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  speak(items[index]['desc']!);
+                },
+                child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Image.asset(
+                          items[index]['image']!,
+                          fit: BoxFit.contain,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        items[index]['desc'],
-                        style: TextStyle(fontSize: 18),
-                        textAlign: TextAlign.center,
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          items[index]['desc']!,
+                          style: TextStyle(fontSize: 18),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        ));
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
